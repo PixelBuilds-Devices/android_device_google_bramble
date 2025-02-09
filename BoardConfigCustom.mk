603bd816453526a27dd43f6ee989c6830a224abc
ExactExampl <xmpl@dontsleep.work>
2025-02-09 14:33:51 +0200

    bramble: 64 bit only build with 32 bit targets
    * Zygote is 64 bit

--- BoardConfigCustom.mk
+++ BoardConfigCustom.mk
@@ -4,6 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Build the 32 bit targets
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a76
+
+# Enable 64-bit for non-zygote.
+ZYGOTE_FORCE_64 := true
+
 # Add before redbull BoardConfigCustom.mk
 BOOT_KERNEL_MODULES += ftm5.ko
 
